9fef459693d8a06e7e75532c95e242c43572ab7e
hamza-halawa2020 <h.halawa2020@gmail.com>
2025-07-09 00:20:10 +0300

    updatat

--- app/services/whatsappService.js
+++ app/services/whatsappService.js
@@ -1,6 +1,7 @@
 const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 const WhatsAppSession = require("../models/WhatsAppSession");
+const puppeteer = require("puppeteer"); 
 
 const clients = new Map();
 
